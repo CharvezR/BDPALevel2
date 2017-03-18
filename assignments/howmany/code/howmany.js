7d2f1d015f20ec42d7f5cb3b57b19b7fa5565782
METROSTATE\36BDPA16 <cjussell123@gmail.com>
2017-03-18 13:36:37 -0500

    commit

--- assignments/howmany/code/howmany.js
+++ assignments/howmany/code/howmany.js
@@ -1,17 +1,37 @@
 function doLevel1(){
-alert ('Do Level 1')
-var answer=Prompt("what's the answer") 
-hide("level1")
-show ("level2")
+	var answer=prompt("what's the answer") 
+	if( answer== "Satellite dishes"){
+		alert("Congratulations you got it right")
+		addPoints(10)
+	} else {
+		alert("Sorry the correct answer was Satellite dishes")
+	}	
+	hide("level1") 
+	show ("level2")
 }
 function doLevel2(){
-alert ('Do Level 2')
+var answer=prompt("what's the answer") 
+if( answer== "5"){
+		alert("Congratulations you got it right")
+		addPoints(10)
+	} else {
+		alert("Sory the correct answer was 4")
+	}	
 hide ("level2")
 show ("level3")
 }
 function doLevel3(){
-alert ('Do Level 3')
+var answer=prompt("what's the answer") 
+if( answer== "true"){
+		alert("Congratulations you got it right")
+		addPoints(10)
+	} else {
+		alert("Sorry the correct answe was true")
+	}	
 hide ("level3")
+show ("Congrats")
+showFinalScore()
+alert ("CONGRAGULATIONS YOU HAVE FINISHED THE GAME THAT I, CHARVEZ RUSSELL HAS CREATED")
 }
 function show(id){
 	var el = document.getElementById(id);
@@ -27,4 +47,6 @@ function addPoints(points){
 	score = score + points;
 	document.getElementById("score").innerHTML = score;
 }
-
+function showFinalScore(){
+	document.getElementById("FinalScore").innerHTML="Final Score="+score;
+}
